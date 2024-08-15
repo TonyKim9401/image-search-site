04241232cfb76e90979f8f295713a9b4df00dbee
ktony <k.tony9412@gmail.com>
2024-08-14 23:42:28 -0400

    feat: Connect to unsplash API
    Using axios library now it can request data from external API. Important data for authorization access key and site address are stored in local json file for security.

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,12 +2,39 @@ import CommonNav from "@/components/common/navigation/CommonNav";
 import CommonHeader from "@/components/common/header/CommonHeader";
 import CommonSearchBar from "@/components/common/searchBar/CommonSearchBar";
 import CommonFooter from "@/components/common/footer/CommonFooter";
-
 import Card from "./components/Card";
+import upsplashData from "./unsplashConfiguration.json";
 
+//CSS
 import styles from "./styles/index.module.scss";
+import axios from "axios";
+import { useEffect } from "react";
 
 function index() {
+  const getData = async () => {
+    // Calling open API
+    const API_URL = upsplashData.API_URL;
+    const API_KEY = upsplashData.API_KEY;
+    console.log(API_KEY);
+    const PER_PAGE = 30;
+
+    const searchValue = "Korea";
+    const pageValue = 100;
+
+    try {
+      const res = await axios.get(
+        `${API_URL}?query=${searchValue}&client_id=${API_KEY}&page=${pageValue}&per_page=${PER_PAGE}`
+      );
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className={styles.page}>
       {/* Common Header UI part */}
