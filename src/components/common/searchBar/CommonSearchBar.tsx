63b3a5b36acd8f63e2ecbe0ee6aebc9ff1f71a07
ktony <k.tony9412@gmail.com>
2024-08-15 19:24:41 -0400

    feat: Navigation pagination add
    Even though there are no instruction how to paginate the result of external API, pagination can be simply implemented by calculating.

--- src/components/common/searchBar/CommonSearchBar.tsx
+++ src/components/common/searchBar/CommonSearchBar.tsx
@@ -1,14 +1,52 @@
-import styles from './CommonSearchBar.module.scss'
+import { useState } from "react";
+import styles from "./CommonSearchBar.module.scss";
+import { useRecoilState } from "recoil";
+import { searchState } from "@/recoil/atoms/searchState";
+import { pageState } from "@/recoil/atoms/pageState";
 
 function CommonSearchBar() {
+  const [search, setSearch] = useRecoilState(searchState);
+  const [page, setPage] = useRecoilState(pageState);
+  const [text, setText] = useState("");
+
+  const onChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const onSearch = () => {
+    createSearchText();
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === "Enter") {
+      createSearchText();
+    }
+  };
+
+  const createSearchText = () => {
+    if (text === "") {
+      setSearch("Canada");
+    } else {
+      setSearch(text);
+    }
+    setPage(1);
+  };
+
   return (
     <div className={styles.searchBar}>
-        <div className={styles.searchBar__search}>
-            <input type="text" placeholder="Search your images" className={styles.searchBar__search__input}/>
-            <img src="src/assets/icons/icon-search.svg" alt="" />
-        </div>
+      <div className={styles.searchBar__search}>
+        <input
+          type="text"
+          placeholder="Search your images"
+          className={styles.searchBar__search__input}
+          value={text}
+          onChange={onChange}
+          onKeyDown={handleKeyDown}
+        />
+        <img src="src/assets/icons/icon-search.svg" alt="" onClick={onSearch} />
+      </div>
     </div>
-  )
+  );
 }
 
-export default CommonSearchBar
+export default CommonSearchBar;
