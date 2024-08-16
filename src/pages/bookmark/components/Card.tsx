b7a176d9997970c8d77142e04a977e0cd8c024e6
ktony <k.tony9412@gmail.com>
2024-08-16 01:29:18 -0400

    feat: Binding data from index to bookmark
    Using props, if there is any saved data in localstorage, it send to bookmark page and spray it out on the page.

--- src/pages/bookmark/components/Card.tsx
+++ src/pages/bookmark/components/Card.tsx
@@ -1,31 +1,42 @@
+import { CardDTO } from "@/pages/index/types/card";
 import styles from "./Card.module.scss";
 
-function Card() {
+interface Props {
+  prop: CardDTO;
+}
+
+function Card({ prop }: Props) {
   return (
     <div className={styles.card}>
       <div className={styles.card__imageBox}>
-        <img src="" alt="" className={styles.card__imageBox__image} />
+        <img
+          src={prop.urls.small}
+          alt=""
+          className={styles.card__imageBox__image}
+        />
       </div>
       <div className={styles.card__infoBox}>
         <div className={styles.card__infoBox__row}>
           <span className={styles.label}>author</span>
-          <span className={styles.value}>value</span>
+          <span className={styles.value}>{prop.user.name}</span>
         </div>
         <div className={styles.card__infoBox__row}>
           <span className={styles.label}>Image size</span>
-          <span className={styles.value}>value</span>
+          <span className={styles.value}>
+            {prop.width} X {prop.height}
+          </span>
         </div>
         <div className={styles.card__infoBox__row}>
           <span className={styles.label}>Upload date</span>
-          <span className={styles.value}>value</span>
+          <span className={styles.value}>{prop.created_at.split("T")[0]}</span>
         </div>
         <div className={styles.card__infoBox__row}>
           <span className={styles.label}>Last update date</span>
-          <span className={styles.value}>value</span>
+          <span className={styles.value}>{prop.updated_at.split("T")[0]}</span>
         </div>
         <div className={styles.card__infoBox__row}>
           <span className={styles.label}>Download Count</span>
-          <span className={styles.value}>value</span>
+          <span className={styles.value}>{prop.likes}</span>
         </div>
       </div>
     </div>
--- src/pages/bookmark/index.tsx
+++ src/pages/bookmark/index.tsx
@@ -2,19 +2,30 @@ import CommonHeader from "@/components/common/header/CommonHeader";
 import styles from "./styles/index.module.scss";
 import { useEffect, useState } from "react";
 import Card from "./components/Card";
+import { CardDTO } from "../index/types/card";
 
 function index() {
   const [data, setData] = useState([]);
-  const getData = () => {};
+
+  const getData = () => {
+    const getLocalStorage = JSON.parse(localStorage.getItem("bookmark"));
+
+    if (getLocalStorage || getLocalStorage !== null) setData(getLocalStorage);
+    else setData([]);
+  };
+
   useEffect(() => {
     getData();
   }, []);
+
   return (
     <div className={styles.page}>
       {/* Common header UI part */}
       <CommonHeader />
       <main className={styles.page__contents}>
-        <Card />
+        {data.map((item: CardDTO) => {
+          return <Card prop={item} key={item.id} />;
+        })}
       </main>
     </div>
   );
